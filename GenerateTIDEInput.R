c5cf3a4e1ea461e3fc4ad63c1e8ccf809f67d149
Karl WU <71440449+KarlWu8500s@users.noreply.github.com>
2022-10-14 14:50:41 +0800

    Update GenerateTIDEInput.R

--- GenerateTIDEInput.R
+++ GenerateTIDEInput.R
@@ -13,20 +13,21 @@ rownames(hcc_fpkm) <- uniquifyFeatureNames(rownames(map),map$gene)
 ### Extract only tumor samples
 hcc_T_fpkm <- hcc_fpkm[,sapply(colnames(hcc_fpkm), function(x) unlist(strsplit(x, split="\\."))[4]) == "01A"]
 
-target.gene <- "FOSL1"
-DrawTIDE <- function()
-order_data_TUMOR <- hcc_T_fpkm[,order(hcc_T_fpkm[target.gene,],decreasing = TRUE)]
-order_data_TUMOR.normalized <- sweep(order_data_TUMOR, 1, rowMeans(order_data_TUMOR))
+GenerateTIDEinput <- function(gene){
+  target.gene <- gene
+  order_data_TUMOR <- hcc_T_fpkm[,order(hcc_T_fpkm[target.gene,],decreasing = TRUE)]
+  order_data_TUMOR.normalized <- sweep(order_data_TUMOR, 1, rowMeans(order_data_TUMOR))
 
-rowMeans(order_data_TUMOR.normalized)
-order_data_TUMOR.normalized[target.gene,]
+  rowMeans(order_data_TUMOR.normalized)
+  order_data_TUMOR.normalized[target.gene,]
 
-order_data.normalized <- order_data_TUMOR.normalized
+  order_data.normalized <- order_data_TUMOR.normalized
 
-first_last_15 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*15/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*15/100)+1):ncol(order_data.normalized))]
-first_last_25 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*25/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*25/100)+1):ncol(order_data.normalized))]
-first_last_30 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*30/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*30/100)+2):ncol(order_data.normalized))]
+  first_last_15 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*15/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*15/100)+1):ncol(order_data.normalized))]
+  first_last_25 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*25/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*25/100)+1):ncol(order_data.normalized))]
+  first_last_30 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*30/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*30/100)+2):ncol(order_data.normalized))]
 
-write.table(first_last_15, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_15_normalized.',target.gene,'.txt'), sep="\t", quote=F)
-write.table(first_last_25, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_25_normalized.',target.gene,'.txt'), sep="\t", quote=F)
-write.table(first_last_30, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_30_normalized.',target.gene,'txt'), sep="\t", quote=F)
+  write.table(first_last_15, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_15_normalized.',target.gene,'.txt'), sep="\t", quote=F)
+  write.table(first_last_25, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_25_normalized.',target.gene,'.txt'), sep="\t", quote=F)
+  write.table(first_last_30, paste0('./output_TIDE/',target.gene,'_order_data_TUMOR.first_last_30_normalized.',target.gene,'txt'), sep="\t", quote=F)
+}
