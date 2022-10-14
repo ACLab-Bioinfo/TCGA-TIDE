f95d8822e6bc8c7c45103e1f49e63772208ef370
Karl WU <71440449+KarlWu8500s@users.noreply.github.com>
2022-10-14 15:29:34 +0800

    Update RunTide.sh

--- scripts/RunTide.sh
+++ scripts/RunTide.sh
@@ -4,9 +4,9 @@ Input_file1="./input_TIDE/${gene}/order_data_TUMOR.first_last_15_normalized.${ge
 Input_file2="./input_TIDE/${gene}/order_data_TUMOR.first_last_25_normalized.${gene}.txt"
 Input_file3="./input_TIDE/${gene}/order_data_TUMOR.first_last_30_normalized.${gene}.txt"
 
-Output_file="./output_TIDE/${gene}/TIDE.output.first_last_15_normalized.${gene}.txt"
-Output_file="./output_TIDE/${gene}/TIDE.output.first_last_25_normalized.${gene}.txt"
-Output_file="./output_TIDE/${gene}/TIDE.output.first_last_30_normalized.${gene}.txt"
+Output_file1="./output_TIDE/${gene}/TIDE.output.first_last_15_normalized.${gene}.txt"
+Output_file2="./output_TIDE/${gene}/TIDE.output.first_last_25_normalized.${gene}.txt"
+Output_file3="./output_TIDE/${gene}/TIDE.output.first_last_30_normalized.${gene}.txt"
 
 tidepy ${Input_file1} -o ${Output_file1} -c Other --ignore_norm
 tidepy ${Input_file2} -o ${Output_file2} -c Other --ignore_norm
