ba1747f0758d07b137059df1782d13d22bfd09f3
Karl WU <71440449+KarlWu8500s@users.noreply.github.com>
2022-10-14 14:57:03 +0800

    Update GenerateTIDEInput.R

--- GenerateTIDEInput.R
+++ GenerateTIDEInput.R
@@ -15,6 +15,8 @@ hcc_T_fpkm <- hcc_fpkm[,sapply(colnames(hcc_fpkm), function(x) unlist(strsplit(x
 
 GenerateTIDEinput <- function(gene){
   target.gene <- gene
+  dir.create(paste0("./", target.gene))
+  
   order_data_TUMOR <- hcc_T_fpkm[,order(hcc_T_fpkm[target.gene,],decreasing = TRUE)]
   order_data_TUMOR.normalized <- sweep(order_data_TUMOR, 1, rowMeans(order_data_TUMOR))
 
@@ -27,7 +29,7 @@ GenerateTIDEinput <- function(gene){
   first_last_25 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*25/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*25/100)+1):ncol(order_data.normalized))]
   first_last_30 <- order_data.normalized[,c(1:(ncol(order_data.normalized)*30/100),round(ncol(order_data.normalized)-(ncol(order_data.normalized)*30/100)+2):ncol(order_data.normalized))]
 
-  write.table(first_last_15, paste0('./input_TIDE/',target.gene,'_order_data_TUMOR.first_last_15_normalized.',target.gene,'.txt'), sep="\t", quote=F)
-  write.table(first_last_25, paste0('./input_TIDE/',target.gene,'_order_data_TUMOR.first_last_25_normalized.',target.gene,'.txt'), sep="\t", quote=F)
-  write.table(first_last_30, paste0('./input_TIDE/',target.gene,'_order_data_TUMOR.first_last_30_normalized.',target.gene,'txt'), sep="\t", quote=F)
+  write.table(first_last_15, paste0('./input_TIDE/',target.gene,'/order_data_TUMOR.first_last_15_normalized.',target.gene,'.txt'), sep="\t", quote=F)
+  write.table(first_last_25, paste0('./input_TIDE/',target.gene,'/order_data_TUMOR.first_last_25_normalized.',target.gene,'.txt'), sep="\t", quote=F)
+  write.table(first_last_30, paste0('./input_TIDE/',target.gene,'/order_data_TUMOR.first_last_30_normalized.',target.gene,'txt'), sep="\t", quote=F)
 }
