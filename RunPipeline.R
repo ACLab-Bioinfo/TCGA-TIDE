2dbd3a26bc188bd1bbdbdee994668f87ebd43ece
Karl WU <71440449+KarlWu8500s@users.noreply.github.com>
2022-11-03 15:28:07 +0800

    Update RunPipeline.R

--- RunPipeline.R
+++ RunPipeline.R
@@ -3,6 +3,23 @@ source("./scripts/GenerateTIDEInput.R")
 source("./scripts/DrawTIDE.R")
 source("./scripts/ModifyTIDEresult.R")
 
+suppressPackageStartupMessages({
+  library(scater)
+  library(tidyverse)
+  library(stringr)
+})
+
+### readDATA
+hcc_fpkm <- read.table(gzfile("./data/TCGA-LIHC.htseq_fpkm.tsv.gz"), header = TRUE, row.names = 1, sep = "\t")
+#hcc_fpkm <- read.table("./data/TCGA-LIHC.htseq_fpkm.tsv", header = TRUE, row.names = 1, sep = "\t")
+map <- read.table("./data/gencode.v22.annotation.gene.probeMap", header=T, row.names=1)
+map <- map[rownames(hcc_fpkm),]
+
+### Annotation
+rownames(hcc_fpkm) <- uniquifyFeatureNames(rownames(map),map$gene)
+### Extract only tumor samples
+hcc_T_fpkm <- hcc_fpkm[,sapply(colnames(hcc_fpkm), function(x) unlist(strsplit(x, split="\\."))[4]) == "01A"]
+
 gene_list <- unlist(read.table("./data/genelist.txt", quote = F))
 
 for(gene in gene_list){
